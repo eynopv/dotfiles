55aafeccf6ffae275edea0594c9eb059149e24a2
eynopv <eynopv@gmail.com>
2022-11-01 10:03:36 +0200

    add remove links script

---
+++ scripts/remove_links.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "about to remove links"
+
+rm ~/.xinitrc
+rm ~/.xserverrc
+
+rm ~/.profile
+rm ~/.bashrc
+
+rm ~/.vimrc
+
+rm ~/.npmrc
+
+rm ~/.zshrc
+
+echo "all links are removed"
