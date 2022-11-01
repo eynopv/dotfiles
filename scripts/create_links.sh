8d714d57fa3e558fdbd9c5d321f170ded75b4150
eynopv <eynopv@gmail.com>
2022-11-01 10:51:12 +0200

    make x and npm optional

--- scripts/create_links.sh
+++ scripts/create_links.sh
@@ -1,16 +1,34 @@
-#!/bin/sh
+#!/bin/bash
+
+use_x=0
+use_npm=0
+
+while :; do
+  case $1 in
+    --use-x) use_x=1
+      ;;
+    --use-npm) use_npm=1
+      ;;
+    *) break
+  esac
+  shift
+done
 
 echo "about to create links"
 
-ln -sf ~/dotfiles/xinitrc     ~/.xinitrc
-ln -sf ~/dotfiles/xserverrc   ~/.xserverrc
+if [[ $use_x -eq 1 ]]; then
+  ln -sf ~/dotfiles/xinitrc     ~/.xinitrc
+  ln -sf ~/dotfiles/xserverrc   ~/.xserverrc
+fi
 
 ln -sf ~/dotfiles/profile     ~/.profile
 ln -sf ~/dotfiles/bashrc      ~/.bashrc
 
 ln -sf ~/dotfiles/vimrc       ~/.vimrc
 
-#ln -sf ~/dotfiles/npmrc       ~/.npmrc
+if [[ $use_npm -eq 1 ]]; then
+  ln -sf ~/dotfiles/npmrc       ~/.npmrc
+fi;
 
 ln -sf ~/dotfiles/zshrc       ~/.zshrc
 
