f653906ee0e67e129929f111339eb32c7c3e24b9
eynopv <eynopv@gmail.com>
2024-01-05 08:59:23 +0200

    add lua formatter

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -1,62 +1,64 @@
 -- Utilities for creating configurations
-local util = require "formatter.util"
+local util = require("formatter.util")
 
 -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-require("formatter").setup {
-  logging = true,
-  log_level = vim.log.levels.DEBUG,
+require("formatter").setup({
+	logging = true,
+	log_level = vim.log.levels.DEBUG,
 
-  filetype = {
+	filetype = {
+		javascriptreact = {
+			require("formatter.filetypes.javascriptreact").prettier,
+		},
 
-    javascriptreact = {
-      require("formatter.filetypes.javascriptreact").prettier,
-    },
+		typescriptreact = {
+			require("formatter.filetypes.typescriptreact").prettier,
+		},
 
-    typescriptreact = {
-      require("formatter.filetypes.typescriptreact").prettier,
-    },
+		typescript = {
+			require("formatter.filetypes.typescript").prettier,
+		},
 
-    typescript = {
-      require("formatter.filetypes.typescript").prettier,
-    },
+		javascript = {
+			require("formatter.filetypes.javascript").prettier,
+		},
 
-    javascript = {
-      require("formatter.filetypes.javascript").prettier,
-    },
+		svelte = {
+			require("formatter.filetypes.svelte").prettier,
+		},
 
-    svelte = {
-      require("formatter.filetypes.svelte").prettier,
-    },
+		jsonc = {
+			require("formatter.filetypes.json").prettier,
+		},
 
-    jsonc = {
-      require("formatter.filetypes.json").prettier,
-    },
+		json = {
+			require("formatter.filetypes.json").prettier,
+		},
 
-    markdown = {
-      require("formatter.filetypes.markdown").prettier,
-    },
+		markdown = {
+			require("formatter.filetypes.markdown").prettier,
+		},
 
-    python = {
-      require("formatter.filetypes.python").black,
-    },
+		python = {
+			require("formatter.filetypes.python").black,
+		},
 
-    --lua = {
-    --  require("formatter.filetypes.lua").luafmt,
-    --},
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+		},
 
-
-    ["*"] = {
-      require("formatter.filetypes.any").remove_trailing_whitespace
-    }
-  }
-}
+		--["*"] = {
+		--  require("formatter.filetypes.any").remove_trailing_whitespace
+		--}
+	},
+})
 
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
-augroup('__formatter__', { clear = true })
-autocmd('BufWritePost', {
-	group = '__formatter__',
-	command = ':FormatWrite',
+augroup("__formatter__", { clear = true })
+autocmd("BufWritePost", {
+	group = "__formatter__",
+	command = ":FormatWrite",
 })
 --autocmd('User', {
 --	group = '__formatter__',
