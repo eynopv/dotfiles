fd4db7a54adce4a5103cbb0596ddb34491aba650
eynopv <eynopv@gmail.com>
2024-03-10 10:51:25 +0200

    add platform tools installation with android sdkmanager

--- scripts/install_android.sh
+++ scripts/install_android.sh
@@ -1,7 +1,17 @@
-cd ~/
+#!/bin/bash
+cd $HOME
+
+echo "Creating Android directory"
 mkdir Android
-cd ~/Android
-wget https:/dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip
-unzip commandlinetools-linux-9477386_latest.zip
+cd Android
 
+# command line tools only, without android studio
+# https://developer.android.com/studio/index.html#command-line-tools-only
+version=commandlinetools-linux-11076708_latest.zip
+echo "Downloading command line tools $version"
+wget "https://dl.google.com/android/repository/$version"
+unzip "$version"
+rm "$version"
 
+echo "Installing platform and build tools"
+$HOME/Android/cmdline-tools/bin/sdkmanager "platform-tools" "platforms;android-26" "build-tools;26.0.3" --sdk_root=$HOME/Android
