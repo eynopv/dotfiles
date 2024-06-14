140c7a4f1544a26efaf89bec36f339798e8f1cd2
eynopv <eynopv@gmail.com>
2024-06-14 14:56:28 +0300

    add zsh themes

--- scripts/install_zsh.sh
+++ scripts/install_zsh.sh
@@ -3,3 +3,5 @@
 exec sudo apt-get install zsh
 exec git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 exec git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+exec git clone --depth=1 https://github.com/migoa/neo ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/neo
+exec git clone --depth=1 https://github.com/rafaelsq/nuts.zsh-theme ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/nuts
