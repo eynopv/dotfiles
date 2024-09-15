9a533f07c8ef6c8692868c28819446350aa28d64
eynopv <eynopv@gmail.com>
2024-09-15 15:30:45 +0300

    make setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install build-essential
+sudo apt-get install fzf
 sudo apt-get install zsh
+
+# zsh plugins
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# zsh themes
 git clone --depth=1 https://github.com/migoa/neo ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/neo
 git clone --depth=1 https://github.com/rafaelsq/nuts.zsh-theme ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/nuts
+
+# set login shell to zsh
 chsh -s $(which zsh)
