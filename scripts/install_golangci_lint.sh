2eee8180d96de1aec4f36aa0997bb7f735498d8e
eynopv <eynopv@gmail.com>
2024-09-16 15:39:09 +0300

    remove unused installation scripts

--- scripts/install_golangci_lint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.59.0
-go install github.com/nametake/golangci-lint-langserver@latest
--- scripts/install_nvm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#https://github.com/nvm-sh/nvm
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
