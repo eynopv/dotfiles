14b5ae0a24b216b48726aa72eab69e3e80731242
eynopv <eynopv@gmail.com>
2024-12-05 16:03:42 +0200

    enable golangcilsp

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,31 +1,31 @@
 local lspconfig = require("lspconfig")
---local configs = require("lspconfig/configs")
+local configs = require("lspconfig/configs")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
---if not configs.golangcilsp then
---	configs.golangcilsp = {
---		default_config = {
---			cmd = { "golangci-lint-langserver" },
---			root_dir = lspconfig.util.root_pattern(".git"),
---			init_options = {
---				command = {
---					"golangci-lint",
---					"run",
---					"--enable-all",
---					"--disable",
---					"lll",
---					"--out-format",
---					"json",
---					"--issues-exit-code=1",
---				},
---			},
---		},
---	}
---end
---lspconfig.golangci_lint_ls.setup({
---	filetypes = { "go", "gomod" },
---	capabilities = capabilities,
---})
+if not configs.golangcilsp then
+	configs.golangcilsp = {
+		default_config = {
+			cmd = { "golangci-lint-langserver" },
+			root_dir = lspconfig.util.root_pattern(".git"),
+			init_options = {
+				command = {
+					"golangci-lint",
+					"run",
+					"--enable-all",
+					"--disable",
+					"lll",
+					"--out-format",
+					"json",
+					"--issues-exit-code=1",
+				},
+			},
+		},
+	}
+end
+lspconfig.golangci_lint_ls.setup({
+	filetypes = { "go", "gomod" },
+	capabilities = capabilities,
+})
 
 lspconfig.pyright.setup({
 	capabilities = capabilities,
@@ -39,9 +39,9 @@ lspconfig.svelte.setup({
 	capabilities = capabilities,
 })
 --lspconfig.eslint.setup({})
-lspconfig.gopls.setup({
-	capabilities = capabilities,
-})
+--lspconfig.gopls.setup({
+--	capabilities = capabilities,
+--})
 
 lspconfig.zls.setup({
 	capabilities = capabilities,
