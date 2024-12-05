e69da661c4c5d1750d19a4df6eccc0674c7517e5
eynopv <eynopv@gmail.com>
2024-12-05 16:04:18 +0200

    add golangcilsp into installation script

--- scripts/go/install.sh
+++ scripts/go/install.sh
@@ -5,10 +5,14 @@ source ../common.sh
 echo "Installing go"
 
 echo "Downloading go"
-curl -L https://go.dev/dl/go1.23.2.linux-amd64.tar.gz | tar -xz -C ~/.local/share || exit 1
+curl -L https://go.dev/dl/go1.23.3.linux-amd64.tar.gz | tar -xz -C ~/.local/share || exit 1
 
 echo "Installing gopls"
 ~/.local/share/go/bin/go install golang.org/x/tools/gopls@latest
+
+echo "Installing golangci-lint"
+~/.local/share/go/bin/go install github.com/nametake/golangci-lint-langserver@latest
+~/.local/share/go/bin/go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 
 if ! grep -Fq "# @go start" "$ZSHRC_LOCAL"; then
   add_block_to_local_zshrc "go" \
