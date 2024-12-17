566b550312feda6ddd454bbb75559b4faad9cba3
eynopv <eynopv@gmail.com>
2024-12-17 16:00:00 +0200

    update pnpm installation script

--- scripts/install_pnpm.sh
+++ scripts/install_pnpm.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+set -eo pipefail
+
 # TODO: PNPM installation script makes changes to zshrc which needs to avoided as our
 # zshrc is shared with other machines
 
 curl -fsSL https://get.pnpm.io/install.sh | sh -
-pnpm env --global lts
+~/.local/share/pnpm/pnpm env use --global lts
+
+# pnpm
+#export PNPM_HOME="/home/eynopv/.local/share/pnpm"
+#case ":$PATH:" in
+#  *":$PNPM_HOME:"*) ;;
+#  *) export PATH="$PNPM_HOME:$PATH" ;;
+#esac
+# pnpm end
