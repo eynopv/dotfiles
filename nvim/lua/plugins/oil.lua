f32c43f02aebb38084e953a86a1203d3f8561b90
eynopv <eynopv@gmail.com>
2024-12-23 15:40:58 +0200

    show hidden files in oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,1 +1,8 @@
-require("oil").setup()
+require("oil").setup({
+	view_options = {
+		show_hidden = true,
+		is_always_hidden = function(name, buffer)
+			return name == ".." or name == ".git"
+		end,
+	},
+})
