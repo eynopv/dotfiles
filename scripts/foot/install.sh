0207989a140b3aee3956bedf684f70145f31498a
eynopv <eynopv@gmail.com>
2025-01-23 15:16:17 +0200

    have foot config directory instead of single file

--- scripts/foot/install.sh
+++ scripts/foot/install.sh
@@ -6,5 +6,5 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 sudo apt install foot foot-themes
 
-mkdir -p ~/.config/foot
-ln -sf $SCRIPT_DIR/foot.ini ~/.config/foot/foot.ini
+mkdir -p ~/.config
+ln -sf $SCRIPT_DIR/config ~/.config/foot
