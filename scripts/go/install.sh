b84125aef6541c6c2aae59eca85b025cae3c7949
eynopv <eynopv@gmail.com>
2025-02-18 08:37:56 +0200

    remove old go version during installation

--- scripts/go/install.sh
+++ scripts/go/install.sh
@@ -4,15 +4,20 @@ source ../common.sh
 
 echo "Installing go"
 
+if [ -d "$HOME/.local/share/go" ]; then
+  echo "Removing old version"
+  rm -rf "$HOME/.local/share/go"
+fi
+
 echo "Downloading go"
-curl -L https://go.dev/dl/go1.24.0.linux-amd64.tar.gz | tar -xz -C ~/.local/share || exit 1
+curl -L https://go.dev/dl/go1.24.0.linux-amd64.tar.gz | tar -xz -C "$HOME/.local/share" || exit 1
 
 echo "Installing gopls"
-~/.local/share/go/bin/go install golang.org/x/tools/gopls@latest
+"$HOME/.local/share/go/bin/go" install golang.org/x/tools/gopls@latest
 
 echo "Installing golangci-lint"
-~/.local/share/go/bin/go install github.com/nametake/golangci-lint-langserver@latest
-~/.local/share/go/bin/go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+"$HOME/.local/share/go/bin/go" install github.com/nametake/golangci-lint-langserver@latest
+"$HOME/.local/share/go/bin/go" install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 
 if ! grep -Fq "# @go start" "$ZSHRC_LOCAL"; then
   add_block_to_local_zshrc "go" \
