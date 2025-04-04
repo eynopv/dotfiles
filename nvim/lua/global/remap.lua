3f3e392d5c1f5c74074dc834757b301651850410
eynopv <eynopv@gmail.com>
2025-04-04 13:06:40 +0300

    feat(nvim): comment out previous lsp bindings in favor of default ones

--- nvim/lua/global/remap.lua
+++ nvim/lua/global/remap.lua
@@ -34,20 +34,36 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local opts = { buffer = ev.buf }
 		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+
+		-- Never used workspaces
+		--vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+		--vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+		--vim.keymap.set("n", "<space>wl", function()
+		--	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		--end, opts)
+
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-		vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-		vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-		vim.keymap.set("n", "<space>wl", function()
-			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-		end, opts)
-		vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-		vim.keymap.set("n", "<space>f", function()
-			vim.lsp.buf.format({ async = true })
-		end, opts)
+		--  default mapping is "C-s"
+		-- vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts)
+
+		-- can just use "gd"
+		-- vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+
+		-- default mapping is "gri"
+		-- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+
+		-- default mapping is "grn"
+		-- vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+
+		-- default mapping is "gra"
+		-- vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+
+		-- default mapping is "grr"
+		-- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+
+		-- I use format on save
+		--vim.keymap.set("n", "<space>f", function()
+		--  vim.lsp.buf.format({ async = true })
+		--end, opts)
 	end,
 })
