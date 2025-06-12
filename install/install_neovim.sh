dca49d5ae92d377be1b8bcca3e0a90a66fe4be80
eynopv <eynopv@gmail.com>
2025-06-12 08:00:07 +0300

    chore(neovim): bump version

--- install/install_neovim.sh
+++ install/install_neovim.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/.local/share/nvim
 
-curl -L https://github.com/neovim/neovim/releases/download/v0.11.1/nvim-linux-x86_64.tar.gz | tar -xz -C ~/.local/share/nvim
+curl -L https://github.com/neovim/neovim/releases/download/v0.11.2/nvim-linux-x86_64.tar.gz | tar -xz -C ~/.local/share/nvim
 
 # plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
