7ba8989b83f43c5d1e99130e4d570291d537588c
eynopv <eynopv@gmail.com>
2025-09-09 13:37:32 +0300

    feat(zshrc): add zsh syntax highlights

--- install/setup.sh
+++ install/setup.sh
@@ -15,6 +15,7 @@ chsh -s $(which zsh)
 # zsh plugins
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # zsh themes
 git clone --depth=1 https://github.com/migoa/neo ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/neo
