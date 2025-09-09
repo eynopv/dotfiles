5f23beadd62d01eb8839b37362c743463c75c92d
eynopv <eynopv@gmail.com>
2025-09-09 18:21:46 +0300

    feat: add tmux installation

---
+++ install/tmux/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eo pipefail
+
+source ../common.sh
+
+MODULE_NAME="tmux"
+
+sudo apt update && sudo apt install -y tmux
+
+if ! has_block_in_zshrc "$MODULE_NAME"; then
+  add_block_to_local_zshrc "$MODULE_NAME" \
+    'if [[ -z "$TMUX" ]]; then' \
+    ' tmux_session_name="main"' \
+    ' if ! tmux has-session -t $tmux_session_name 2>/dev/null; then' \
+    '   tmux new-session -d -s $tmux_session_name -n "dev" \; \' \
+    '     split-window -h -l 65% \; \' \
+    '     split-window -v -t 0 \; \' \
+    '     select-pane -t 2' \
+    '   tmux attach-session -t $tmux_session_name' \
+    ' fi' \
+    'fi'
+fi
