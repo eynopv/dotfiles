f95a6631ae177dfe7d1cac07b4ace10935104587
eynopv <eynopv@gmail.com>
2026-04-15 21:11:30 +0300

    fix: use correct upsert method

--- install/neovim/install.sh
+++ install/neovim/install.sh
@@ -29,7 +29,7 @@ if [ ! -d "$HOME/.local/share/nvim/site/autoload/plug.vim" ]; then
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 
-upsert_block_to_local_zshrc $MODULE_NAME \
+upsert_block_to_zshrc $MODULE_NAME \
   "export PATH=\$HOME/.local/share/nvim/$PACKAGE_NAME/bin:\$PATH" \
   "export PATH=\$HOME/.local/share/lua-ls/bin:\$PATH" \
   'alias vim="nvim"' \
