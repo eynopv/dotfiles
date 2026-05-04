59a398915b140f136528a4a5101e9ba415665346
eynopv <eynopv@gmail.com>
2026-05-04 22:22:04 +0300

    done stuff

--- sandboxes/sandbox.sh
+++ sandboxes/sandbox.sh
@@ -2,7 +2,37 @@
 
 set -eo pipefail
 
+show_help() {
+    cat << EOF
+Usage: $(basename "$0") [IMAGE]
+
+IMAGE:
+    pi      Use pi image
+    gemini  Use gemini image
+EOF
+}
+
+image=""
+
+if [[ -n "$1" ]]; then
+  case "$1" in
+    pi|gemini)
+      image="$1"
+      ;;
+    -h|--help)
+      show_help
+      exit 0
+      ;;
+    *)
+      echo "Error: Unknown image '$1'"
+      echo ""
+      show_help
+      exit 1
+      ;;
+  esac
+fi
+
 docker run --rm -it \
   -e TERM=$TERM \
   -v "$HOME/.pi":/home/coder/.pi \
-  pi
+  "$image"
