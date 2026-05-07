63b960173a5e9a74392f8f1364f6091ea12e220f
eynopv <eynopv@gmail.com>
2026-05-07 14:55:20 +0300

    feat(packages): add macos support to go install, refactor and bump version

--- install/go/install.sh
+++ install/go/install.sh
@@ -2,18 +2,23 @@
 
 set -eo pipefail
 
-SCRIPT_PATH="$(realpath -- "${BASH_SOURCE[0]}")"
-SCRIPT_DIR="$(dirname -- "$SCRIPT_PATH")"
+version=1.26.2
 
-source "$SCRIPT_DIR/../common.sh"
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../common.sh"
 
-if [ -d "$HOME/.local/share/go" ]; then
+MODULE_NAME=$(module_name)
+
+if [[ -d "$HOME/.local/share/go" ]]; then
   echo "Removing old version"
   rm -rf "$HOME/.local/share/go"
 fi
 
 echo "Downloading go"
-curl -L https://go.dev/dl/go1.26.0.linux-amd64.tar.gz | tar -xz -C "$HOME/.local/share" || exit 1
+if [[ is_macos ]]; then
+  curl -L https://go.dev/dl/go${version}.darwin-arm64.tar.gz | tar -xz -C "$HOME/.local/share" || exit 1
+else
+  curl -L https://go.dev/dl/go${version}.linux-amd64.tar.gz | tar -xz -C "$HOME/.local/share" || exit 1
+fi
 
 echo "Installing gopls"
 "$HOME/.local/share/go/bin/go" install golang.org/x/tools/gopls@latest
@@ -22,8 +27,6 @@ echo "Installing golangci-lint"
 "$HOME/.local/share/go/bin/go" install github.com/nametake/golangci-lint-langserver@latest
 "$HOME/.local/share/go/bin/go" install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.10.1
 
-if ! grep -Fq "# @go start" "$ZSHRC_LOCAL"; then
-  add_block_to_local_zshrc "go" \
-    "export PATH=\$HOME/.local/share/go/bin:\$PATH" \
-    "export PATH=\$HOME/go/bin:\$PATH"
-fi
+upsert_block_to_zshrc "$MODULE_NAME" \
+  "export PATH=\$HOME/.local/share/go/bin:\$PATH" \
+  "export PATH=\$HOME/go/bin:\$PATH"
