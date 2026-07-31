2f2204f66555397404067111f5eba976997c92f1
eynopv <eynopv@gmail.com>
2026-07-31 17:08:02 +0300

    fix(packages): force remove directory if it does not exist

--- install/neovim/install.sh
+++ install/neovim/install.sh
@@ -57,7 +57,7 @@ build_lua_ls_package_name() {
 
 # Language servers
 LUA_LS_VERSION="3.18.2"
-rm -r ~/.local/share/lua-ls
+rm -rf ~/.local/share/lua-ls
 mkdir -p ~/.local/share/lua-ls
 curl -L "https://github.com/LuaLS/lua-language-server/releases/download/$LUA_LS_VERSION/$(build_lua_ls_package_name)" | tar -xz -C ~/.local/share/lua-ls
 
