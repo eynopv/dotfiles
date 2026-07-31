e4bb5afb4bc60d10abd270965b4d65749be13a4e
eynopv <eynopv@gmail.com>
2026-07-31 17:05:09 +0300

    fix(packages): ensure .local/bin directories exist

--- install/treesitter/install.sh
+++ install/treesitter/install.sh
@@ -4,7 +4,9 @@ set -eo pipefail
 
 VERSION="v0.26.7"
 
+mkdir -p ~/.local/bin
 
 curl -L "https://github.com/tree-sitter/tree-sitter/releases/download/$VERSION/tree-sitter-linux-x64.gz" | gunzip > ~/.local/bin/tree-sitter
 
 chmod +x ~/.local/bin/tree-sitter
+
