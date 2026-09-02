d3cea98378f379ceb247f4f338d0c13d7a6703c0
eynopv <eynopv@gmail.com>
2026-09-02 10:42:15 +0300

    feat(packages): add mac support for aws-cli install

--- install/aws-cli/install.sh
+++ install/aws-cli/install.sh
@@ -1,17 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -eo pipefail
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../common.sh"
 
-if [ -d "/usr/local/aws-cli" ]; then
-  echo "Found existing installation. Updating"
-  sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+if [[ is_mac ]]; then
+  brew install awscli
 else
-  sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli
-fi
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
 
-# Cleanup
-rm awscliv2.zip
-rm -r aws
+  if [[ -d "$HOME/.local/share/aws-cli" ]]; then
+    echo "Found existing installation. Updating"
+    sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+  else
+    sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli
+  fi
+
+  # Cleanup
+  rm awscliv2.zip
+  rm -r aws
+fi
