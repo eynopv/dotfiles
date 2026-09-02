3146ee0fef8f575313b292c1b9bff2453dd440af
eynopv <eynopv@gmail.com>
2026-09-02 20:28:31 +0300

    chore(packages): use latest tmux on linux

--- install/tmux/install.sh
+++ install/tmux/install.sh
@@ -7,8 +7,14 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../common.sh"
 SCRIPT_DIR=$(script_dir)
 MODULE_NAME=$(module_name)
 
-pkg_update
-pkg_install tmux
+if is_linux; then
+  echo "LINUX"
+  tlink="https://github.com/tmux/tmux-builds/releases/download/v3.7c/tmux-3.7c-linux-x86_64.tar.gz"
+  curl -L "$tmux_link" | tar -xz -C ~/.local/bin/
+else
+  pkg_update
+  pkg_install tmux
+fi
 
 upsert_block_to_zshrc "$MODULE_NAME" \
   'if [[ -z "$TMUX" ]]; then' \
@@ -21,6 +27,5 @@ upsert_block_to_zshrc "$MODULE_NAME" \
   '   tmux -u attach-session -t $tmux_session_name' \
   ' fi' \
   'fi'
-
 
 create_symlink $SCRIPT_DIR/tmux.conf $HOME/.tmux.conf
